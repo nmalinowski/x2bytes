f1b6fee19e7a60b7aa81df02d33535c6bd3ab886
Nathaniel Malinowski <nmalinowski@users.noreply.github.com>
2018-07-11 18:07:23 -0500

    Add files via upload

--- X2Bytes.py
+++ X2Bytes.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import os
-import system
 
 tobytes = \
     raw_input('Enter the value you need converted from char to bytes: ')
-len(tobytes.encode('utf8'))
+converted = len(tobytes.encode('utf8'))
 
-
-def pause():
-    programPause = raw_input('Press the <ENTER> key to continue...')
-
-
-pause()
-
-			
+print converted
