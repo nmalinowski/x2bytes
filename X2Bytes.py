86a9982b06d339c515c81521c72eb17b42781ced
Nathaniel Malinowski <nmalinowski@users.noreply.github.com>
2018-07-24 20:31:59 -0500

    Update X2Bytes.py

--- X2Bytes.py
+++ X2Bytes.py
@@ -7,3 +7,16 @@ tobytes = \
 converted = len(tobytes.encode('utf8'))
 
 print converted
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+tobytes = \
+    input('Enter the value you need converted from char to bytes: ')
+converted = len(tobytes.encode('utf8'))
+print('')
+print('')
+sys.stdout.write('Length of String in Bytes: ')
+print(converted)
+sys.stdout.flush
