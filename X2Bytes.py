1eef4583ab24d1672bd9f534d130eb329e3f37bf
Nathaniel Malinowski <nmalinowski@users.noreply.github.com>
2018-07-24 20:32:41 -0500

    Update X2Bytes.py

--- X2Bytes.py
+++ X2Bytes.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import os
-
-tobytes = \
-    raw_input('Enter the value you need converted from char to bytes: ')
-converted = len(tobytes.encode('utf8'))
-
-print converted
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-import os
 import sys
 
 tobytes = \
